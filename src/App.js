5c25b3732d638a6573609cb273975ace7527c676
Reverie22 <franco.campaas@gmail.com>
2022-11-05 17:36:15 -0300

    Primeros cambios en la app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './Imagenes/logo.png';
-import './App.css';
+import './Css/App.css';
 
 function App() {
+  setTimeout(CambiarPantalla() => {
+    
+  }, timeout);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
       </header>
+      <body>
+      <h3 class="Titulo-Booky">¡Booky!</h3>
+      </body>
     </div>
   );
 }
