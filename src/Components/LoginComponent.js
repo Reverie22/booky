f3bfc5a48f82da6b8bd7d47a6de58a0b417e1bf3
Reverie22 <franco.campaas@gmail.com>
2022-11-12 17:31:25 -0300

    Agregando estilos y logo al login

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -1,4 +1,5 @@
-
+import logo from '../Imagenes/logo.png';
+    
 import '../Css/LoginScreenCSS.css';
 import React from 'react';
 
@@ -6,22 +7,24 @@ const Formulario = () => {
 	return (
 		
     <div className='fondo'>
+    
 			<form>
-        <div>
-          <label htmlFor='nombre'>Nombre</label>
-          <input type='text' id='nombre'name='nombre' placeholder='Su nombre aqui...'/>
+      <img src={logo} className='App-logo' alt="logo" />
+        <div className='campos'>
+          <label className='labels' htmlFor='nombre'>Nombre</label>
+          <input className='inputs' type='text' id='nombre'name='nombre' placeholder='Su nombre aqui...'/>
         </div>
-        <div>
-          <label htmlFor='Apellido'>Apellido</label>
-          <input type='text' id='Apellido'name='Apellido' placeholder='Su apellido aqui...'/>
+        <div className='campos'>
+          <label className='labels' htmlFor='Apellido'>Apellido</label>
+          <input className='inputs' type='text' id='Apellido'name='Apellido' placeholder='Su apellido aqui...'/>
         </div>
-        <div>
-          <label htmlFor='correo'>correo</label>
-          <input type='email' id='correo'name='correo' placeholder='Su@correoaqui.com'/>
+        <div className='campos'>
+          <label className='labels' htmlFor='correo'>correo</label>
+          <input className='inputs' type='email' id='correo'name='correo' placeholder='Su@correoaqui.com'/>
         </div>
-        <div>
-          <label htmlFor='contraseña'>Contraseña</label>
-          <input type='password' id='contraseña'name='contraseña' placeholder='Su contraseña aqui...'/>
+        <div className='campos'>
+          <label className='labels' htmlFor='contraseña'>Contraseña</label>
+          <input className='inputs' type='password' id='contraseña'name='contraseña' placeholder='Su contraseña aqui...'/>
         </div>
         <button type='submit'>Enviar</button>
       </form>
