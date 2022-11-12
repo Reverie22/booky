f7601afc61c5992e511a3a78973114919b90a5bc
Reverie22 <franco.campaas@gmail.com>
2022-11-12 16:17:59 -0300

    Agregando SplashScreen

---
+++ src/Screens/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SplashScreenComponent from "../Components/SplashScreenComponent";
+
+
+function App() {
+    return (
+  
+    <SplashScreenComponent/>
+      
+    );
+  }
+  
+  export default App;
