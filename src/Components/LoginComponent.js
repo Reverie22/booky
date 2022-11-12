508caf63015488356ba428195b3deea8c01c6d14
Reverie22 <franco.campaas@gmail.com>
2022-11-12 18:21:39 -0300

    Mejorando COdigo y cargando funcion submit

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -5,26 +5,51 @@ import { Formik } from 'formik';
 
 const Formulario = () => {
 	return (
-    <Formik>
-		{()=> (
- <div className='fondo'>
+    <Formik
+    onSubmit={()=>{
+      console.log('form enviado');
+
+
+    }}>
+		{( handleSubmit)=> (
+ <div className='fondo' onSubmit={handleSubmit}>
  <form>
  <img src={logo} className='logo' alt="logo" />
    <div className='campos'>
      <label className='labels' htmlFor='nombre'>Nombre</label>
-     <input className='inputs' type='text' id='nombre'name='nombre' placeholder='Su nombre aqui...'/>
+     <input 
+     className='inputs' 
+     type='text' 
+     id='nombre'
+     name='nombre'
+      placeholder='Su nombre aqui...'/>
    </div>
    <div className='campos'>
      <label className='labels' htmlFor='Apellido'>Apellido</label>
-     <input className='inputs' type='text' id='Apellido'name='Apellido' placeholder='Su apellido aqui...'/>
+     <input 
+     className='inputs' 
+     type='text' 
+     id='Apellido'
+     name='Apellido' 
+     placeholder='Su apellido aqui...'/>
    </div>
    <div className='campos'>
      <label className='labels' htmlFor='correo'>correo</label>
-     <input className='inputs' type='email' id='correo'name='correo' placeholder='Su@correoaqui.com'/>
+     <input 
+     className='inputs' 
+     type='email' 
+     id='correo'
+     name='correo' 
+     placeholder='Su@correoaqui.com'/>
    </div>
    <div className='campos'>
      <label className='labels' htmlFor='contraseña'>Contraseña</label>
-     <input className='inputs' type='password' id='contraseña'name='contraseña' placeholder='Su contraseña aqui...'/>
+     <input 
+     className='inputs' 
+     type='password' 
+     id='contraseña'
+     name='contraseña' 
+     placeholder='Su contraseña aqui...'/>
    </div>
    <button type='submit'>Enviar</button>
  </form>
