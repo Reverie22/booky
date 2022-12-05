2bfb1ddf515fd9350ce130e70748756e964ea0f1
Reverie22 <franco.campaas@gmail.com>
2022-12-04 22:24:40 -0300

    Agregando imagen, y rehaciendo estilos

--- src/Components/HomeScreenComponent.js
+++ src/Components/HomeScreenComponent.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import '../Css/HomeScreenCSS.css';
+import ImagenBanner from '../Imagenes/ImagenBanner.jpg';
 
 const HomeScreenComponent = () => {
   return (
     <div className='Contenedor'>
     <header>
-    <div className='Navbar'>
+    
       <h1 className='TituloBar'>¡Booky!</h1>
+      <div className='Navbar'>
       <a href='/LoginScreen'>Opciones</a>
       <a href='/Login'>Cursos</a>
       <a href='/Login'>Inicio</a>
     </div>
     </header>
-    
+    <div className='Cuerpo'>
+    <img src={ImagenBanner} className='Banner' alt="Banner" />
+    <div className='Info'>
+      <p>Bienvenido! textotexto textotexto  textotexto  textotexto  textotexto  textotexto  textotexto  textotexto  textotexto  textotexto    </p>
+    </div>
+    </div>
     
     </div>
   )
---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+export default {
+    
+   
+    apiKey: "AIzaSyDaVwuhfpl1J5zwMMZfjdCByJROrr0POmI",
+    authDomain: "bookyx-b7425.firebaseapp.com",
+    databaseURL: "https://bookyx-b7425-default-rtdb.firebaseio.com",
+    projectId: "bookyx-b7425",
+    storageBucket: "bookyx-b7425.appspot.com",
+    messagingSenderId: "928179007145",
+    appId: "1:928179007145:web:c87f1be8141b1271df03b8"
+  };
